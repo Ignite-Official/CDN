b9330637c726228e606c9a8fe67467948f9fc922
Jaeseong Yoo <praster1@gmail.com>
2023-05-07 01:02:27 +0900

    포트폴리오 스크립트 파일 통합

--- web/scripts/index/me.js
+++ web/scripts/index/me.js
@@ -1,3 +1,69 @@
+/***********************************
+ 프로필 / 포트폴리오 메뉴 선택 스크립트
+ ***********************************/
+let toggle_sub_menu_portfolio = 1
+let toggle_sub_menu_profile = 0
+
+
+// 포트폴리오 수정하기 / 프로필 수정하기 버튼
+function Div_btn_botton(props) {
+	return(
+		<button type="button" onClick={props.function}
+				class="bg-[#340BB8] text-[18px] text-white font-[600] w-[328px] h-[48px] rounded-lg text-center mt-[150px]">
+			{props.text}
+		</button>
+	)
+}
+
+// 프로필 / 포트폴리오 메뉴 선택 스크립트
+function sub_menu_selection(mode = "portfolio") {
+	let class_div_active = "pt-[16px]"
+	let class_div_inactive = "pt-[16px] hidden"
+	let class_menu_active = "flex justify-center items-center w-[180px] h-[24px] pb-[12px] border-b-4 border-white text-white cursor-pointer"
+	let class_menu_inactive = "flex justify-center items-center w-[180px] h-[24px] pb-[12px] text-[#4B4B4B] cursor-pointer"
+
+	if (mode == "portfolio") {
+		document.getElementById("div_portfolio").className = class_div_active
+		document.getElementById("div_profile").className = class_div_inactive
+		document.getElementById("sub_menu_portfolio").className = class_menu_active
+		document.getElementById("sub_menu_profile").className = class_menu_inactive
+		toggle_sub_menu_portfolio = 1
+		toggle_sub_menu_profile = 0
+
+	} else if (mode == "profile") {
+		document.getElementById("div_portfolio").className = class_div_inactive
+		document.getElementById("div_profile").className = class_div_active
+		document.getElementById("sub_menu_portfolio").className = class_menu_inactive
+		document.getElementById("sub_menu_profile").className = class_menu_active
+		toggle_sub_menu_portfolio = 0
+		toggle_sub_menu_profile = 1
+		
+	}
+}
+
+
+/***********************************
+ 프로필 정보 스크립트
+ ***********************************/
+
+// 팀업 토글
+function Div_toggle_teamup(props) {
+	return(
+		<div class="flex flex-col justify-center items-center">
+			<label class="relative inline-flex items-center cursor-pointer">
+				<input type="checkbox" id="toggle_teamup" value={props.teamup_available} class="sr-only peer" checked={props.checked}/>
+				<div class="w-11 h-6 bg-gray-200 rounded-full peer
+							peer-focus:ring-4 peer-focus:ring-[#37B24D]
+							peer-checked:after:translate-x-full peer-checked:after:border-white peer-checked:bg-[#37B24D]
+							after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border 
+							after:rounded-full after:h-5 after:w-5 after:transition-all">
+				</div>
+			</label>
+			<span class="text-[#8B8B8B] text-[12px] font-[500] mt-[4px]">{props.text}</span>
+		</div>
+	)
+}
+
 async function get_profile_info() {
 	// 헤더
 	function Div_header(props) {
@@ -40,7 +106,7 @@ async function get_profile_info() {
 				<div class="flex flex-col justify-center items-center">
 					<div class="flex flex-row justify-center items-center space-x-[4px]">
 						<img src="https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/emoji/fire_1f525.png" 
-							 class="w-[20px] h-[20px]" />
+							class="w-[20px] h-[20px]" />
 						<span class="text-[#FF6B6B] text-[16px] font-[700]">0</span>
 					</div>
 		
@@ -73,27 +139,33 @@ async function get_profile_info() {
 		
 		
 				<div class="flex flex-col justify-center items-center">
-					<div>
-						<label class="relative inline-flex items-center cursor-pointer">
-							<input type="checkbox" value="" class="sr-only peer" />
-							<div class="w-11 h-6 bg-gray-200 rounded-full peer
-										peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#37B24D]
-										peer-checked:after:translate-x-full peer-checked:after:border-white peer-checked:bg-[#37B24D]
-										after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all">
-							</div>
-							</label>
-					</div>
-		
-					<div>
-						<span class="text-[#8B8B8B] text-[12px] font-[500]">팀업 가능</span>
-					</div>
-		
-					<div class="flex flex-row items-center text-[#40C057] text-[11px] font-[500] space-x-[4px] hidden">
-						<span>팀업 가능</span>
-						<svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<circle cx="3" cy="3" r="3" fill="#40C057"/>
-						</svg>
-					</div>
+						{
+							// 다른 사람의 포트폴리오에 들어왔을 경우
+							props.data.self_portfolio == "FALSE"
+							? ( props.data.teamup_available == "YES"
+								? <div class="flex flex-row items-center text-[#40C057] text-[11px] font-[500] space-x-[4px] hidden">
+									  <span>팀업 가능</span>
+									  <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+										  <circle cx="3" cy="3" r="3" fill="#40C057"/>
+									  </svg>
+								  </div>
+								: <div class="flex flex-row items-center text-[#8C6BF7] text-[11px] font-[500] space-x-[4px] hidden">
+									  <span>팀업 불가</span>
+									  <svg width="6" height="6" viewBox="0 0 6 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+										  <circle cx="3" cy="3" r="3" fill="#8C6BF7"/>
+									  </svg>
+								  </div>
+							  )
+							// 자기 자신의 포트폴리오에 들어왔을 경우
+							: ( props.data.teamup_available == "YES"
+								? <Div_toggle_teamup teamup_available={props.data.teamup_available}
+													 checked={true}
+													 text="팀업 가능" />
+								: <Div_toggle_teamup teamup_available={props.data.teamup_available}
+													 checked={false}
+													 text="팀업 불가" />
+							  )
+						}
 				</div>
 			</div>
 		)
@@ -113,7 +185,7 @@ async function get_profile_info() {
 					<div class="flex flex-row justify-between items-center text-start">
 						<div class="flex flex-row justify-between items-center">
 							<img src="https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/emoji/bust-in-silhouette_1f464.png"
-								 class="w-[20px] h-[20px] mr-[6px]" />
+								class="w-[20px] h-[20px] mr-[6px]" />
 							<h2 class="text-[14px] font-[700] text-white">저를 소개해요.</h2>
 						</div>
 					</div>
@@ -130,7 +202,7 @@ async function get_profile_info() {
 					<div class="flex flex-row justify-between items-center text-start">
 						<div class="flex flex-row justify-between items-center">
 							<img src="https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/emoji/guitar_1f3b8.png"
-								 class="w-[20px] h-[20px] mr-[6px]" />
+								class="w-[20px] h-[20px] mr-[6px]" />
 							<h2 class="text-[14px] font-[700] text-white">이런 음악을 만들고 싶어요.</h2>
 						</div>
 					</div>
@@ -147,7 +219,7 @@ async function get_profile_info() {
 					<div class="flex flex-row justify-between items-center text-start">
 						<div class="flex flex-row justify-between items-center">
 							<img src="https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/emoji/raising-hands_1f64c.png"
-								 class="w-[20px] h-[20px] mr-[6px]" />
+								class="w-[20px] h-[20px] mr-[6px]" />
 							<h2 class="text-[14px] font-[700] text-white">이런 분과 협업하고 싶어요.</h2>
 						</div>
 					</div>
@@ -172,15 +244,6 @@ async function get_profile_info() {
 		)
 	}
 
-	function Div_button(props) {
-		return (
-			<button type="button"
-					class="bg-[#340BB8] text-[18px] text-white font-[600] w-[328px] h-[48px] rounded-lg text-center mt-[150px]">
-				{props.text}
-			</button>
-		)
-	}
-
 
 	// 현재 경로
 	const request_data = new FormData();
@@ -188,14 +251,12 @@ async function get_profile_info() {
 
 	// 프로필 데이터 가져오기
 	const data = await fetch("/get_portfolio_info/", {
-					   method: "post", 
-					   headers: {
-						   "X-CSRFToken": getCookie("csrftoken"),
-					   },
-					   body: request_data
-					   })
-					  .then(res=> { return res.json(); })
-					  .then(res=> { return res; });
+					method: "post", 
+					headers: { "X-CSRFToken": getCookie("csrftoken"), },
+					body: request_data
+					})
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
 
 	console.log(data)
 	ReactDOM.render(<Div_header data={data} />, document.getElementById("div_header"))
