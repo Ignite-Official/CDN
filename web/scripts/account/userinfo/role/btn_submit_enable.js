54ec45c5e62a1a986d2f85587541c536ce950fde
Jaeseong Yoo <praster1@gmail.com>
2023-05-21 03:23:38 +0900

    역할 > 완료 버튼 활성화 조건

---
+++ web/scripts/account/userinfo/role/btn_submit_enable.js
@@ -0,0 +1,11 @@
+function btn_submit_enable() {
+	if (role_selected_list.length > 0) {
+		ReactDOM.render(<Div_btn_submit class={class_btn_submit_enabled}
+										text={btn_text} 
+										function={() => submit()} />, document.getElementById("div_btn_submit"))
+	} else {
+		ReactDOM.render(<Div_btn_submit class={class_btn_submit_disabled}
+										text={btn_text} 
+										function={null} />, document.getElementById("div_btn_submit"))
+	}
+}
