fed2ac18f6956af96512b14cd92c935cc4d8f70d
Jaeseong Yoo <praster1@gmail.com>
2023-05-20 20:09:51 +0900

    뷰 컬럼명 변경에 따른 스크립트 변경 (sex -> gender)

---
+++ web/scripts/account/userinfo/get_userinfo_list_20230520_2008.js
@@ -0,0 +1,24 @@
+async function get_userinfo_list() {   
+
+	function Div_label(props) {
+		return (
+			<span class="text-[#BCBCBC] text-[11px] font-[500]">
+				{props.text}
+			</span>
+		)
+	}
+
+	const data = await fetch("/account/ajax_get_userinfo_menu/")
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	ReactDOM.render(<Div_label text={data.account} />, document.getElementById("lbl_account"));
+	if (data.birthday == "9999.12.31") {
+		ReactDOM.render(<Div_label text="Unknown" />, document.getElementById("lbl_birthday"));
+	} else {
+		ReactDOM.render(<Div_label text={data.birthday} />, document.getElementById("lbl_birthday"));
+	}
+	ReactDOM.render(<Div_label text={data.gender} />, document.getElementById("lbl_gender"));
+}
+
+get_userinfo_list()
