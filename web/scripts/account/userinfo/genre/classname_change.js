8661e1d71f91c8039b319796444bd22a412315d5
Jaeseong Yoo <praster1@gmail.com>
2023-05-21 03:02:03 +0900

    장르 > 버튼 form 스크립트 분리

---
+++ web/scripts/account/userinfo/genre/classname_change.js
@@ -0,0 +1,16 @@
+function classname_change(id) {
+	let class_focused = "rounded-lg w-[98px] h-[98px] bg-account-genre-focus focus:bg-center mb-[14px] ml-[8px] mr-[8px] hover:bg-account-genre-hover hover:bg-center focus:bg-[#5F5F5F] justify-items-center text-white"
+	let class_not_focused = "rounded-lg w-[98px] h-[98px] bg-[#5F5F5F] focus:bg-center mb-[14px] ml-[8px] mr-[8px] hover:bg-account-genre-hover hover:bg-center focus:bg-[#5F5F5F] justify-items-center text-white"
+
+	// 이미 선택된 장르인 경우
+	if (genre_selected_list.includes(id)) {
+		document.getElementById(id).className = class_not_focused
+		genre_selected_list = genre_selected_list.filter((element) => element !== id)
+
+	} else if (genre_selected_list.length < 3) {
+			document.getElementById(id).className = class_focused
+			genre_selected_list.push(id)
+	}
+
+	btn_submit_enable()
+}
--- web/scripts/account/userinfo/genre/insert_genre.js
+++ web/scripts/account/userinfo/genre/insert_genre.js
@@ -55,3 +55,30 @@ function genre_form_check() {
 		return true
 	}
 }
+
+
+async function insert_genre() {
+	if ( toggle_insert_genre == 1) {
+		// 텍스트박스에 입력한 값 (장르)
+		let genre = document.getElementById("txt_genre_add").value.trim()
+
+		const data = await fetch("/account/ajax_insert_genre/?genre=" + genre)
+						.then(res=> { return res.json(); })
+						.then(res=> { return res; });
+
+		if (data.CNT == 0) {
+			const button = Object.assign(document.createElement('button'))
+			button.id = data.uuid
+			button.name = data.uuid
+			button.className = "rounded-lg w-[98px] h-[98px] bg-[#5F5F5F] mb-[14px] ml-[8px] mr-[8px] "
+										+ "justify-items-center text-white "
+										+ "hover:bg-account-genre-hover hover:bg-center "
+										+ "focus:bg-[#5F5F5F]"
+			button.innerText = data.name
+			button.onclick = () => classname_change(data.uuid)
+			document.getElementById("div_genre_btn_list_sub").appendChild(button)
+		}
+			
+		classname_change(data.uuid)
+	}
+}
