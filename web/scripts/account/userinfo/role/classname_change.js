aa36e0cdc33766f717060b71a05ac7132b0d37cc
Jaeseong Yoo <praster1@gmail.com>
2023-05-21 03:20:23 +0900

    클릭 후 classname 변경 스크립트 분리

---
+++ web/scripts/account/userinfo/role/classname_change.js
@@ -0,0 +1,47 @@
+// 역할 - 클릭 후 버튼 classname 변경 (배경색 변경)
+function classname_change_role(id, image_url) {
+	let class_focused = "border border-[#4B4B4B] rounded-lg w-[160px] h-[88px] ml-[4px] mr-[4px] mb-[8px] "
+						+ "bg-cover text-white text-[20px] font-[500] focus:bg-[#26009F] "
+						+ "bg-" + image_url + "-hover"
+						+ " hover:bg-" + image_url + "-hover"
+						+ " focus:bg-" + image_url + "-hover"
+	let class_not_focused = "border border-[#4B4B4B] rounded-lg w-[160px] h-[88px] ml-[4px] mr-[4px] mb-[8px] "
+						+ "bg-cover text-white text-[20px] font-[500] focus:bg-[#26009F] "
+						+ "bg-" + image_url 
+						+ " hover:bg-" + image_url + "-hover"
+						+ " focus:bg-" + image_url
+
+	// 이미 선택된 장르인 경우
+	if (role_selected_list.includes(id)) {
+		document.getElementById(id).className = class_not_focused
+		role_selected_list = role_selected_list.filter((element) => element !== id)
+
+	} else {
+		document.getElementById(id).className = class_focused
+		role_selected_list.push(id)
+	}
+}
+
+// 악기 - 클릭 후 버튼 classname 변경 (배경색 변경)
+function classname_change_instrument(id, image_url) {
+	let class_focused = "rounded-lg border border-[#5F5F5F] "
+						+ "w-fit max-w-[150px] h-[48px] pt-[10px] pb-[10px] pl-[19.5px] pr-[19.5px] ml-[8px] mb-[8px] "
+						+ "bg-[#292929] text-white "
+						+ "hover:bg-[#292929] hover:text-white "
+						+ "focus:bg-[#292929] focus:text-white"
+	let class_not_focused = "rounded-lg border border-[#5F5F5F] "
+						+ "w-fit max-w-[150px] h-[48px] pt-[10px] pb-[10px] pl-[19.5px] pr-[19.5px] ml-[8px] mb-[8px] "
+						+ "bg-transparent text-[#5F5F5F] "
+						+ "hover:bg-[#292929] hover:text-white "
+						+ "bg-transparent text-[#5F5F5F]"
+
+	// 이미 선택된 장르인 경우
+	if (instrument_selected_list.includes(id)) {
+		document.getElementById(id).className = class_not_focused
+		instrument_selected_list = instrument_selected_list.filter((element) => element !== id)
+
+	} else {
+		document.getElementById(id).className = class_focused
+		instrument_selected_list.push(id)
+	}
+}
