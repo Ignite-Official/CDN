b6fe943c1d05b6d2fd457297a00c750b3b5ce636
Jaeseong Yoo <praster1@gmail.com>
2023-05-21 14:35:59 +0900

    버튼 hover 설정 수정

---
+++ web/scripts/account/userinfo/gender/btn_submit_enable_20230521_1435.js
@@ -0,0 +1,44 @@
+// 선택한 성별
+let selected_gender = "Unknown"
+
+// 성별 선택
+function select_btn_gender(mode) {
+	const class_btn_default = "rounded-full w-[328px] h-[48px] text-[18px] font-[500] "
+	const class_btn_male = class_btn_default + "bg-[#26009F] text-white hover:bg-[#26009F] hover:text-white focus:bg-[#26009F] focus:text-white"
+	const class_btn_female = class_btn_default + "bg-[#600298] text-white hover:bg-[#600298] hover:text-white focus:bg-[#600298] focus:text-white"
+	const class_btn_male_inactive = class_btn_default + "bg-[#292929] text-[#8B8B8B] hover:bg-[#292929] hover:text-[#8B8B8B] focus:bg-[#26009F] focus:text-white"
+	const class_btn_female_inactive = class_btn_default + "bg-[#292929] text-[#8B8B8B] hover:bg-[#292929] hover:text-[#8B8B8B] focus:bg-[#600298] focus:text-white"
+	
+	selected_gender = mode
+	// 남성
+	if (mode == "male") {
+		document.getElementById("btn_gender_male").className = class_btn_male
+		document.getElementById("btn_gender_female").className = class_btn_female_inactive
+
+	// 여성
+	} else if (mode == "female") {
+		document.getElementById("btn_gender_male").className = class_btn_male_inactive
+		document.getElementById("btn_gender_female").className = class_btn_female
+
+	// 알 수 없음
+	} else {
+		document.getElementById("btn_gender_male").className = class_btn_male_inactive
+		document.getElementById("btn_gender_female").className = class_btn_female_inactive
+
+	}
+
+}
+
+
+// 성별이 입력되어 있어야 버튼이 활성화된다.
+function btn_submit_enable() {
+	if (selected_gender != "") {
+		ReactDOM.render(<Div_btn_submit class={class_btn_submit_enabled}
+										text={btn_text} 
+										function={() => submit()} />, document.getElementById("div_btn_submit"))
+	} else {
+		ReactDOM.render(<Div_btn_submit class={class_btn_submit_disabled}
+										text={btn_text} 
+										function={null} />, document.getElementById("div_btn_submit"))
+	}
+}
