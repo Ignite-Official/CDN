0ada832d32364ca2627b07957b0ad86c18949387
Jaeseong Yoo <praster1@gmail.com>
2023-05-21 15:35:11 +0900

    생년월일 > is_active 스크립트를 함수별로 분리

---
+++ web/scripts/account/userinfo/birthday/is_active/get_birthday.js
@@ -0,0 +1,13 @@
+// 생년월일 가져오기
+async function get_birthday() {
+	const data = await fetch("/account/ajax_get_birthday/")
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	if (data.birthday != "9999.12.31") {
+		document.getElementById('txt_birthday').value = data.birthday
+	}
+	btn_submit_enable()
+}
+
+get_birthday()
