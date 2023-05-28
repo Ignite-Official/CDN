a9f7e552246eb0e0a3b8f36fa408a751733b8df9
Jaeseong Yoo <praster1@gmail.com>
2023-05-28 15:12:36 +0900

    console.log 삭제

---
+++ web/scripts/index/me/btn_bottom_change_20230528_1510.js
@@ -0,0 +1,13 @@
+function btn_bottom_change() {
+	if (toggle_btn_bottom == 1 && toggle_sub_menu_portfolio == 1) {
+        let loc = window.location.href.toString() + "/edit_portfolio/"
+		ReactDOM.render(<Div_btn_botton class={class_btn}
+                                        text="포트폴리오 수정하기"
+                                        function={() => location.href=loc} />, document.getElementById("btn_bottom"))
+	} else if (toggle_btn_bottom == 1 && toggle_sub_menu_profile == 1) {
+        let loc = window.location.href.toString() + "/edit_profile/"
+		ReactDOM.render(<Div_btn_botton class={class_btn}
+                                        text="프로필 수정하기"
+                                        function={() => location.href=loc} />, document.getElementById("btn_bottom"))
+	}
+}
