e59f2b45e868e6481aafe5be2f0470d23549dfd0
Jaeseong Yoo <praster1@gmail.com>
2023-05-29 15:52:08 +0900

    스크립트에서 필요없는 인자(url) 제거

---
+++ web/scripts/insert_click_portfolio_20230529_1550.js
@@ -0,0 +1,16 @@
+// 포트폴리오 클릭 로그 기록
+async function insert_click_portfolio(uuid_portfolio) {
+	const request_data = new FormData();
+	request_data.append('uuid_portfolio', uuid_portfolio);
+	request_data.append('host', window.location.href.toString());
+
+	const data = await fetch("/blank/ajax_insert_click_portfolio_log/", {
+								method: "post", 
+								headers: { "X-CSRFToken": getCookie("csrftoken"), },
+								body: request_data
+								})
+		.then(res=> { return res.json(); })
+		.then(res=> { return res; });
+
+	window.open(data.url)
+}
