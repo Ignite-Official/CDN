22c16e897dc07a6d4ca6c2cf2a4a54ae28c1bbb7
Jaeseong Yoo <praster1@gmail.com>
2023-06-06 22:55:00 +0900

    포트폴리오 추가/완료 버튼의 className

---
+++ web/scripts/index/me/init_variables_20230606_2254.js
@@ -0,0 +1,8 @@
+let toggle_sub_menu_portfolio = 1
+let toggle_sub_menu_profile = 0
+let toggle_btn_bottom = 0
+
+let class_btn = "bg-[#340BB8] text-[18px] text-white font-[600] w-[328px] h-[48px] rounded-lg text-center mt-[150px]"
+
+let class_btn_add_mode = "bg-[#340BB8] text-[18px] text-white font-[600] w-[160px] h-[48px] rounded-lg text-center"
+let class_btn_add_mode_closed = "bg-[#3B3B3B] text-[18px] text-white font-[600] w-[160px] h-[48px] rounded-lg text-center cursor-not-allowed"
