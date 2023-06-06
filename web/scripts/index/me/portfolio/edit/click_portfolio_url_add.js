432db4cf049901e4047f5493abd06d02a46e10b4
Jaeseong Yoo <praster1@gmail.com>
2023-06-06 23:15:05 +0900

    포트폴리오 추가/취소 show/hide 스크립트

---
+++ web/scripts/index/me/portfolio/edit/click_portfolio_url_add.js
@@ -0,0 +1,3 @@
+function click_portfolio_url_add() {
+	ReactDOM.render(<Div_btn_bottom_portfolio_edit />, document.getElementById("btn_bottom"))
+}
---
+++ web/scripts/index/me/portfolio/edit/click_portfolio_url_close.js
@@ -0,0 +1,3 @@
+function click_portfolio_url_close() {
+	ReactDOM.render(<Div_btn_bottom_portfolio />, document.getElementById("btn_bottom"))
+}
