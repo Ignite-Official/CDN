f190b996330d7c29f0b91b4967f8cbc5aae252b9
Jaeseong Yoo <praster1@gmail.com>
2023-06-10 20:38:15 +0900

    email, mode를 전역변수로 변경

---
+++ web/scripts/index/me/portfolio/edit/Div_portfolio_edit_20230610_2034.js
@@ -0,0 +1,38 @@
+// 포트폴리오 리스트 (수정 모드)
+function Div_portfolio_edit(props) {
+	
+	const portfolio_list = Object.keys(props.data).map((btn_data) =>
+		<div class="flex flex-col justify-center items-center pb-[50px]">
+			<div role="status" onClick={() => insert_click_portfolio(props.data[btn_data].uuid)}
+				 class={"flex items-center justify-center w-[360px] max-w-[360px] h-[128px] rounded-lg cursor-pointer "
+					  + "bg-[url('" + props.data[btn_data].url_thumbnail +"')] bg-cover bg-center"}>
+				<img src="https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/svg/play_btn_video.svg" class="w-[40px] h-[40px] rounded-lg" />
+			</div>
+
+			<div class="flex justify-between items-center w-[360px] max-w-[360px] mt-[19px]">
+				<div class="flex justify-between items-center w-[360px] max-w-[360px] h-[20px] mb-[15px]"></div>
+
+				{current_email == props.email
+				 ? <Div_btn_portfolio_edit uuid={props.data[btn_data].uuid} />
+				 : ""
+				}
+			</div>
+
+			<div class="flex flex-col justify-start items-start w-[360px] max-w-[360px] mt-[15px]">
+				<span onClick={() => insert_click_portfolio(props.data[btn_data].uuid)}
+					class="text-white text-[14px] font-[700] w-[360px] max-w-[360px] truncate ...">
+					{props.data[btn_data].title}
+				</span>
+	
+				<span class="text-[#BCBCBC] text-[11px] font-[500] mt-[8px] w-[360px] max-w-[360px] whitespace-pre-wrap multi-line-truncate-4">
+					{props.data[btn_data].description}
+				</span>
+			</div>
+		</div>
+	)
+	return (
+		<div class="flex flex-col justify-center items-center pb-[150px]">
+			{portfolio_list}
+		</div>
+	)
+}
---
+++ web/scripts/index/me/portfolio/get_mypage_portfolio_info_20230610_2033.js
@@ -0,0 +1,38 @@
+async function get_mypage_portfolio_info() {
+	// 현재 경로
+	const request_data = new FormData();
+	request_data.append('host', window.location.href.toString());
+
+	// 프로필 데이터 가져오기
+	const data = await fetch("/get_mypage_portfolio_info/", {
+					method: "post", 
+					headers: { "X-CSRFToken": getCookie("csrftoken"), },
+					body: request_data
+					})
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	// 포트폴리오 주인의 이메일
+	let data_email = data.email
+	delete data.email
+	
+	// 포트폴리오 수정하기 모드
+	if (mode == "edit_portfolio") {
+		ReactDOM.render(<Div_portfolio_edit data={data} email={data_email} />, document.getElementById("div_portfolio"));   
+
+		// 내 포트폴리오에 들어왔을 때만 수정하기 버튼이 나타난다.
+		if (current_email == data_email) {
+			ReactDOM.render(<Div_btn_bottom_portfolio />, document.getElementById("btn_bottom"))
+		}
+
+	
+	} else if (mode == "edit_profile") {
+		// 프로필 수정 모드에서는 아무 일도 하지 않는다.
+
+
+	// 일반 모드
+	} else {
+		ReactDOM.render(<Div_portfolio data={data} />, document.getElementById("div_portfolio"));   
+		
+	}
+}
