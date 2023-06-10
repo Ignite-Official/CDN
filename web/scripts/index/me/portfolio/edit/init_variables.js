43ee20abc788c5bc836160972efffec057658645
Jaeseong Yoo <praster1@gmail.com>
2023-06-10 17:55:09 +0900

    포트폴리오 수정 모드의 초기 변수

---
+++ web/scripts/index/me/portfolio/edit/init_variables.js
@@ -0,0 +1,4 @@
+// 포트폴리오 추가에서 source 선택 관련 변수
+let class_btn_portfolio_url_source_enabled = "w-[45px] h-[45px] flex justify-center items-center focus:outline-none bg-white rounded-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700"
+let class_btn_portfolio_url_source_disabled = "w-[45px] h-[45px] flex justify-center items-center focus:outline-none bg-gray-500 rounded-md border border-gray-500 hover:bg-gray-100 hover:text-blue-700"
+let toggle_portfolio_url_source = "youtube"
