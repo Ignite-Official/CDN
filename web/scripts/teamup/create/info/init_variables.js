b61ec5c634c3fb9e1c89d7eb175df69161400928
Jaeseong Yoo <praster1@gmail.com>
2023-06-18 23:30:18 +0900

    초기 변수

---
+++ web/scripts/teamup/create/info/init_variables.js
@@ -0,0 +1,13 @@
+// 각 메뉴의 헤더
+let class_header_title = "text-white w-full font-[600] text-[16px] ml-[16px]"
+let class_header_selection_enabled = "flex justify-end text-end w-[45px] text-[#40C057] font-[500] text-[11px]"
+let class_header_selection_disabled = "flex justify-end text-end w-[45px] text-[#8B8B8B] font-[500] text-[11px]"
+let class_div_content = "w-[380px] h-fit px-[16px] py-[12px] border-b-2 border-[#292929]"
+let img_accordian_up = "https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/teamup/create/accordian_up.svg"
+let img_accordian_down = "https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/teamup/create/accordian_down.svg"
+
+let toggle_div_content_region = 0
+let toggle_div_content_gender = 0
+let toggle_div_content_genre = 0
+let toggle_div_content_role = 0
+let toggle_div_content_due_date = 0
