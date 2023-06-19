a106cfcc18445475c9a6c7a39194195a65b8fc7e
Jaeseong Yoo <praster1@gmail.com>
2023-06-19 23:41:31 +0900

    헤더의 up/down 순서 변경

---
+++ web/scripts/teamup/create/info/header/render_Div_header_by_condition_20230619_2341.js
@@ -0,0 +1,18 @@
+function render_Div_header_by_condition(id, toggle_selection, toggle_content, name) {
+	let img = img_accordian_up
+	if (toggle_content == 1) {
+		img = img_accordian_down
+	}
+
+	if (toggle_selection == 0) {
+		ReactDOM.render(<Div_header title={name}
+									class_selection={class_header_selection_disabled} 
+									selection_name={txt_header_selection_disabled} accrodian_image={img} />,
+									document.getElementById(id))
+	} else {
+		ReactDOM.render(<Div_header title={name}
+									class_selection={class_header_selection_enabled} 
+									selection_name={txt_header_selection_enabled} accrodian_image={img} />,
+									document.getElementById(id))
+	}
+}
