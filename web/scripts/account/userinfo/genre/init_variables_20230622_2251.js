e2a988009683c738bb59c2a20d5de6b8dcc8f048
Jaeseong Yoo <praster1@gmail.com>
2023-06-22 22:53:43 +0900

    같은 이름의 장르가 계속 출력되는 버그 수정

---
+++ web/scripts/account/userinfo/genre/init_variables_20230622_2251.js
@@ -0,0 +1,10 @@
+// 화면에 출력된 장르를 저장하는 리스트
+let genre_list = []
+// 선택한 장르를 저장하는 리스트
+let genre_selected_list = []
+
+// 장르 버튼 form
+let class_focused = "rounded-lg w-[98px] h-[98px] bg-account-genre-focus focus:bg-center mb-[14px] ml-[8px] mr-[8px] hover:bg-account-genre-hover hover:bg-center focus:bg-[#5F5F5F] justify-items-center text-white"
+let class_not_focused = "rounded-lg w-[98px] h-[98px] bg-[#5F5F5F] focus:bg-center mb-[14px] ml-[8px] mr-[8px] hover:bg-account-genre-hover hover:bg-center focus:bg-[#5F5F5F] justify-items-center text-white"
+
+const class_btn_genre_add = "rounded-lg rounded w-[324px] h-[48px] bg-[#5F5F5F] text-white text-[14px] hover:bg-[#340BB8]"
