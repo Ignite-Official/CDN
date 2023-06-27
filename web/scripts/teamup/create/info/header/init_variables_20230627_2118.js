398169b09b20c8754f689b0ddee0c29fe37c264b
Jaeseong Yoo <praster1@gmail.com>
2023-06-27 21:19:24 +0900

    header의 selection 토글을 분리

---
+++ web/scripts/teamup/create/info/header/init_variables_20230627_2118.js
@@ -0,0 +1,19 @@
+// 각 메뉴의 헤더
+let class_header = "w-[380px] h-[48px]  cursor-pointer"
+let class_header_title = "text-white w-full font-[600] text-[16px] ml-[16px]"
+let class_div_content = "w-[380px] h-fit px-[16px] py-[12px] border-b-2 border-[#292929]"
+let img_accordian_up = "https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/teamup/create/accordian_up.svg"
+let img_accordian_down = "https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/teamup/create/accordian_down.svg"
+
+// 메뉴 show/hide 토글
+let toggle_div_content_region = 0
+let toggle_div_content_gender = 0
+let toggle_div_content_genre = 0
+let toggle_div_content_role = 0
+let toggle_div_content_due_date = 0
+
+// 메뉴 selection 표시
+let class_header_selection_enabled = "flex justify-end text-end w-[45px] text-[#40C057] font-[500] text-[11px]"
+let class_header_selection_disabled = "flex justify-end text-end w-[45px] text-[#8B8B8B] font-[500] text-[11px]"
+let txt_header_selection_enabled = "선택"
+let txt_header_selection_disabled = "비선택"
