6154b180c76c74ad47f0de0769130c208162ab03
Jaeseong Yoo <praster1@gmail.com>
2023-06-27 22:44:59 +0900

    bug fix

---
+++ web/scripts/teamup/create/info/region/classname_toggle_selection_region.js
@@ -0,0 +1,18 @@
+function classname_toggle_selection_region() {
+	// 헤더의 선택/비선택 표시 변경
+	if (list_region_selected.length > 0 || toggle_region_online == 1) {
+		ReactDOM.render(<Div_header title={'활동 지역'} 
+									class_selection={class_header_selection_enabled} 
+									selection_name={txt_header_selection_enabled} 
+									accrodian_image={img_accordian_down} />,document.getElementById("div_header_region"));
+		toggle_header_selection_toggle_region = 1
+
+	} else {
+		ReactDOM.render(<Div_header title={'활동 지역'} 
+									class_selection={class_header_selection_disabled} 
+									selection_name={txt_header_selection_disabled} 
+									accrodian_image={img_accordian_down} />,document.getElementById("div_header_region"));
+		toggle_header_selection_toggle_region = 0
+
+	}
+}
---
+++ web/scripts/teamup/create/info/region/click_toggle_online_teamup_20230627_2244.js
@@ -0,0 +1,11 @@
+function click_toggle_online_teamup() {
+	if (toggle_region_online == 0) {
+		toggle_region_online = 1
+	} else {
+		toggle_region_online = 0
+	}
+
+	ReactDOM.render(<Div_toggle_online_teamup />, document.getElementById("div_teamup"));
+
+	classname_toggle_selection_region()
+}
