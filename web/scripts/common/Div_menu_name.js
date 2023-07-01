54e89d2b6933ff3234750332799aaf2d10377855
Jaeseong Yoo <praster1@gmail.com>
2023-07-02 02:40:00 +0900

    Div_menu_name

---
+++ web/scripts/common/Div_menu_name.js
@@ -0,0 +1,7 @@
+function Div_menu_name(props) {
+	return(
+		<span class={props.class}>
+			{props.name}
+		</span>
+	)
+}
--- web/scripts/common/visible_back_button_20230702_0237.js
+++
@@ -1,9 +0,0 @@
-function visible_back_button(document = document, url = "") {
-	if (url == "") {
-		document.getElementById("menu_back").href=url
-	} else {
-		document.getElementById("menu_back").href=document.referrer
-	}
-	
-	document.getElementById("menu_back").className="mr-[8px]"
-}
