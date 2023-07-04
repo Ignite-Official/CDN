db02d79913b6cbb3d3c5f19f36fb68e50d9112d1
Jaeseong Yoo <praster1@gmail.com>
2023-07-04 19:29:44 +0900

    init_variables 추가

---
+++ web/scripts/teamup/create/info/header/init_variables_20230704_1928.js
@@ -0,0 +1,29 @@
+// 각 메뉴의 헤더
+let class_header = "w-[380px] h-[48px]  cursor-pointer"
+let class_header_title = "text-white w-full font-[600] text-[16px] ml-[16px]"
+let class_div_content = "w-[380px] h-fit px-[16px] py-[12px] border-b-2 border-[#292929]"
+let img_accordian_up = "https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/teamup/create/accordian_up.svg"
+let img_accordian_down = "https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/teamup/create/accordian_down.svg"
+
+// 메뉴 show/hide 토글
+let toggle_div_content_region = 0
+let toggle_div_content_gender = 0
+let toggle_div_content_genre = 0
+let toggle_div_content_role = 0
+let toggle_div_content_due_date = 0
+
+// 메뉴 selection 표시
+let class_header_selection_enabled = "flex justify-end text-end w-[45px] text-[#40C057] font-[500] text-[11px]"
+let class_header_selection_disabled = "flex justify-end text-end w-[45px] text-[#8B8B8B] font-[500] text-[11px]"
+let txt_header_selection_enabled = "선택"
+let txt_header_selection_disabled = "비선택"
+
+// 버튼 classname
+let class_btn_enabled = "w-[82px] h-[40px] bg-[#292929] border-2 border-white rounded-lg text-white font-[500] text-[14px] ml-[2px] mr-[2px] mb-[8px]"
+let class_btn_disabled = "w-[82px] h-[40px] bg-transparent border-2 border-[#5F5F5F] rounded-lg text-[#5F5F5F] font-[500] text-[14px] ml-[2px] mr-[2px] mb-[8px]"
+
+// 팝업 관련 옵션
+const popup_options = {
+	backdrop: 'dynamic',
+	closable: false
+}
