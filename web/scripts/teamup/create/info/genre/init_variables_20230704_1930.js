2f6f9f102b52f3c757e62eff4a48ecc1aad000b9
Jaeseong Yoo <praster1@gmail.com>
2023-07-04 19:31:55 +0900

    popup options를 함수 안으로 내재화

---
+++ web/scripts/teamup/create/info/genre/init_variables_20230704_1930.js
@@ -0,0 +1,5 @@
+let list_genre_all = []
+let list_genre_selected = []
+let toggle_header_selection_toggle_genre = 0
+
+let popup_genre_add = new Modal(document.getElementById('popup_genre_add'), {backdrop: 'dynamic', closable: false})
