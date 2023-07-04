3d177d2aa2981a828d13db24b849ed3c2c3d8578
Jaeseong Yoo <praster1@gmail.com>
2023-07-04 21:45:57 +0900

    init variable 추가

---
+++ web/scripts/teamup/create/info/genre/init_variables_20230704_2145.js
@@ -0,0 +1,6 @@
+let list_genre_all = []
+let list_genre_selected = []
+let check_txt_popup_genre_add_toggle = 0
+let toggle_header_selection_toggle_genre = 0
+
+let popup_genre_add = new Modal(document.getElementById('popup_genre_add'), {backdrop: 'dynamic', closable: false})
