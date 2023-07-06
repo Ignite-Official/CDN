4b04fecccc740e42a3e68ac3ec53f571f3d16344
Jaeseong Yoo <praster1@gmail.com>
2023-07-07 02:23:40 +0900

    선택한 멤버 요청 수

---
+++ web/scripts/teamup/create/info/role/init_variables_20230707_0223.js
@@ -0,0 +1,15 @@
+let list_role_all = []
+let list_role_selected = []
+let list_cnt_role_selected = []
+let check_txt_popup_role_add_toggle = 0
+let toggle_header_selection_toggle_role = 0
+
+let popup_role_add = new Modal(document.getElementById('popup_role_add'), {backdrop: 'dynamic', closable: false})
+
+
+let list_instrument_all = []
+let list_instrument_selected = []
+let check_txt_popup_instrument_add_toggle = 0
+let toggle_header_selection_toggle_instrument = 0
+
+let popup_instrument_add = new Modal(document.getElementById('popup_instrument_add'), {backdrop: 'dynamic', closable: false})
