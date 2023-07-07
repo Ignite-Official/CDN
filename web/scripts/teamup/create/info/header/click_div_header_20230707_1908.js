bc269fcf9dbfdfffe2b10a06a43854104abd4aef
Jaeseong Yoo <praster1@gmail.com>
2023-07-07 19:08:57 +0900

    bug fix

---
+++ web/scripts/teamup/create/info/header/click_div_header_20230707_1908.js
@@ -0,0 +1,78 @@
+function click_div_header(div_id = "") {
+	if (div_id == "div_content_region") {
+		if (toggle_div_content_region == 0) {
+			document.getElementById("div_content_region").className = class_div_content
+			ReactDOM.render(<Div_content_loading />,document.getElementById(div_id))
+			toggle_div_content_region = 1
+			show_div_content_region()
+			
+		} else {
+			document.getElementById("div_content_region").className = "hidden"
+			toggle_div_content_region = 0
+		}
+
+	} else {
+		document.getElementById("div_content_region").className = "hidden"
+		toggle_div_content_region = 0
+	}
+	
+	if (div_id == "div_content_gender") {
+		if (toggle_div_content_gender == 0) {
+			document.getElementById("div_content_gender").className = class_div_content
+			ReactDOM.render(<Div_content_loading />,document.getElementById(div_id))
+			toggle_div_content_gender = 1
+			show_div_content_gender()
+		} else {
+			document.getElementById("div_content_gender").className = "hidden"
+			toggle_div_content_gender = 0
+		}
+	} else {
+		document.getElementById("div_content_gender").className = "hidden"
+		toggle_div_content_gender = 0
+	}
+	
+	if (div_id == "div_content_genre") {
+		if (toggle_div_content_genre == 0) {
+			document.getElementById("div_content_genre").className = class_div_content
+			ReactDOM.render(<Div_content_loading />,document.getElementById(div_id))
+			toggle_div_content_genre = 1
+			show_div_content_genre()
+		} else {
+			document.getElementById("div_content_genre").className = "hidden"
+			toggle_div_content_genre = 0
+		}
+	} else {
+		document.getElementById("div_content_genre").className = "hidden"
+		toggle_div_content_genre = 0
+	}
+	
+	if (div_id == "div_content_role") {
+		if (toggle_div_content_role == 0) {
+			document.getElementById("div_content_role").className = class_div_content
+			ReactDOM.render(<Div_content_loading />,document.getElementById(div_id))
+			toggle_div_content_role = 1
+			show_div_content_role()
+		} else {
+			document.getElementById("div_content_role").className = "hidden"
+			toggle_div_content_role = 0
+		}
+	} else {
+		document.getElementById("div_content_role").className = "hidden"
+		toggle_div_content_role = 0
+	}
+	
+	if (div_id == "div_content_due_date") {
+		if (toggle_div_content_due_date == 0) {
+			document.getElementById("div_content_due_date").className = class_div_content
+			toggle_div_content_due_date = 1
+		} else {
+			document.getElementById("div_content_due_date").className = "hidden"
+			toggle_div_content_due_date = 0
+		}
+	} else {
+		document.getElementById("div_content_due_date").className = "hidden"
+		toggle_div_content_due_date = 0
+	}
+
+	show_init_accordian()
+}
