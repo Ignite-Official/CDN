003c238fdeb0544df703f9447ff9c6973e760b3c
Jaeseong Yoo <praster1@gmail.com>
2023-07-15 15:58:27 +0900

    team_list loading

---
+++ web/scripts/teamup/team/Div_team_list_loading.js
@@ -0,0 +1,9 @@
+function Div_team_list_loading() {
+	return (
+		<div class="animate-pulse">
+			<div class="bg-[#292929] w-full rounded-xl h-[134px] mb-[2px] cursor-pointer"></div>
+			<div class="bg-[#292929] w-full rounded-xl h-[134px] mb-[2px] cursor-pointer"></div>
+			<div class="bg-[#292929] w-full rounded-xl h-[134px] mb-[2px] cursor-pointer"></div>
+		</div>
+	)
+}
