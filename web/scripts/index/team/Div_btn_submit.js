b6647bc0543981e49658061a2c398b3b00233ad4
Jaeseong Yoo <praster1@gmail.com>
2023-07-16 11:31:12 +0900

    팀업 요청하기 버튼

---
+++ web/scripts/index/team/Div_btn_submit.js
@@ -0,0 +1,8 @@
+// form 완료 버튼
+function Div_btn_submit(props) {
+	return (
+		<button type="button" id="btn_submit" onClick={props.function} class={props.class}>
+			{props.text}
+		</button>
+	)
+}
