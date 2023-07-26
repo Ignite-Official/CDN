038fd6d26af2e7d694db86b5874a71571e92fdb5
Jaeseong Yoo <praster1@gmail.com>
2023-07-27 00:49:30 +0900

    정렬 방법을 변경하였을 경우 화면 초기화되지 않는 버그 수정

---
+++ web/scripts/teamup/artist/get_artist_list_20230727_0048.js
@@ -0,0 +1,33 @@
+async function get_artist_list(sort_method = "date_joined", div_id = "div_artist_list") {
+	// 정렬 방법이 바뀐 경우
+	if (sort_method != val_sort_method) {
+		val_sort_method = sort_method
+		val_offset = 0
+		change_btn_class(sort_method)
+		ReactDOM.render(<Div_artist_list_loading />, document.getElementById(div_id));
+	}
+
+	const requestData = new FormData();
+	requestData.append('sort_method', val_sort_method);
+	requestData.append('limit', val_limit);
+	requestData.append('offset', val_offset);
+
+	const data = await fetch("/teamup/ajax_get_artist_list/", {
+								method: "post", 
+								headers: {"X-CSRFToken": getCookie("csrftoken"),},
+								body: requestData
+							})
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	val_offset = val_limit + val_offset
+	if (Object.keys(data).length != 0) {
+		ReactDOM.render(<Div_artist_list data={data} page={val_offset}/>, document.getElementById(div_id));
+	} else {
+		try {
+			document.getElementById(div_id).remove();
+		} catch(e) {
+			
+		}
+	}
+}
