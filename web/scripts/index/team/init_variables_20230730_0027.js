ee7d2d1dda2e6d63b612f42762aacb8e1dd3c157
Jaeseong Yoo <praster1@gmail.com>
2023-07-30 00:27:32 +0900

    font size 변경

---
+++ web/scripts/index/team/init_variables_20230730_0027.js
@@ -0,0 +1,3 @@
+let class_btn_submit_enabled = "bg-[#340BB8] text-[16px] text-white font-[600] h-[48px] rounded-lg text-center mb-[-128px]"
+let class_btn_submit_alert = "bg-[#FA5252] text-[16px] text-white font-[600] h-[48px] rounded-lg text-center mb-[-128px]"
+let class_btn_submit_disabled = "bg-[#3B3B3B] text-[16px] text-[#727272] font-[600] h-[48px] rounded-lg text-center mb-[-128px]"
