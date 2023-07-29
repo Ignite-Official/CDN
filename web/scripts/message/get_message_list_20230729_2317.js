01b3eaf0d24745958ceba14a0febb8435cabf472
Jaeseong Yoo <praster1@gmail.com>
2023-07-29 23:18:23 +0900

    console.log 제거

---
+++ web/scripts/message/get_message_list_20230729_2317.js
@@ -0,0 +1,9 @@
+async function get_message_list() {
+	ReactDOM.render(<Div_team_list_loading />, document.getElementById("div_message_list"))
+
+	const data = await fetch("/message/ajax_get_message_list/")
+		.then(res=> { return res.json(); })
+		.then(res=> { return res; });
+	
+	ReactDOM.render(<Div_message_list data={data} />, document.getElementById("div_message_list"))
+}
