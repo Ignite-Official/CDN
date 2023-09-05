846dbd80655e61181813c42ccd84a8694cc8caee
Jaeseong Yoo <praster1@gmail.com>
2023-09-05 21:07:53 +0900

    디바이스별 screensize 정의

---
+++ web/scripts/tailwind_config_20230905_2106.js
@@ -0,0 +1,42 @@
+tailwind.config = {
+	theme: {
+		screens: {
+			'xl': {'max': '1920px'},
+			'lg': {'max': '1280px'},
+			'md': {'max': '960px'},
+			'sm': {'max': '600px'},
+			'xs': {'max': '360px'},
+		},
+		extend: {
+			colors: {
+				backgroundblack: '#191919'
+			},
+			backgroundImage: {
+				'account-signin-email': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/account/signin_email.svg')",
+				'account-signin-email-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/account/signin_email_hover.svg')",
+				'account-signin-kakao': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/account/signin_kakao.svg')",
+				'account-signin-kakao-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/account/signin_kakao_hover.svg')",
+				'account-signin-google': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/account/signin_google.svg')",
+				'account-signin-google-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/account/signin_google_hover.svg')",
+
+				'account-gradient': "radial-gradient(148.05% 148.05% at 113.31% -10.06%, #568FFC 0%, #2E0BB8 48.67%, #9000B4 92.71%)",
+				
+				'account-genre-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/userinfo/genre_card_violet_gradient.svg')",
+				'account-genre-focus': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/account/userinfo/genre_card_violet_logo_20230508_2149.svg')",
+
+				'account-role-vocal': "linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_vocal.png')",
+				'account-role-vocal-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_vocal.png')",
+				'account-role-composition': "linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_composition.png')",
+				'account-role-composition-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_composition.png')",
+				'account-role-engineer': "linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_engineer.png')",
+				'account-role-engineer-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_engineer.png')",
+				'account-role-play': "linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_play.png')",
+				'account-role-play-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_play.png')",
+				'account-role-rapper': "linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_rapper.png')",
+				'account-role-rapper-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_rapper.png')",
+				'account-role-writer': "linear-gradient(0deg, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_writer.png')",
+				'account-role-writer-hover': "url('https://cdn.jsdelivr.net/gh/Ignite-Official/CDN/web/image/role/img_writer.png')"
+			}
+		}
+	}
+}
