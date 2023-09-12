4294213e19f56cf41822dbd2241e7425fd0a63a7
Jaeseong Yoo <praster1@gmail.com>
2023-09-12 23:29:39 +0900

    첫 화면 skeleton

---
+++ web/scripts/index/Div_skeleton_collaborator.js
@@ -0,0 +1,22 @@
+function Div_skeleton_collaborator() {
+	return (
+		<div class="bg-gray-500 rounded-lg w-[144px] min-w-[144px] h-[160px]">
+
+			<div class="flex justify-between pb-[34px]">
+				<div class="bg-gray-400 rounded-full w-[32px] h-[32px] mt-[10px] ml-[10px] border-[2px] border-[#A4A4A4] border-solid"
+					 alt="profile picture"></div>
+			</div>
+		
+			<div class="bg-gray-400 rounded-full ml-[10px] mr-[10px] mb-[6px] w-[68px] max-w-[68px] h-[14px]"></div>
+		
+			<div class="flex flex-row space-x-[5px] ml-[10px] mb-[12px]">
+				<button type="button" class="w-[20px] h-[20px] bg-gray-400 rounded-md"></button>
+				<button type="button" class="w-[20px] h-[20px] bg-gray-400 rounded-md"></button>
+				<button type="button" class="w-[36px] h-[20px] bg-gray-400 rounded-md"></button>
+			</div>
+			<div class="ml-[10px]">
+				<div class="bg-gray-400 rounded-full w-1/2 h-[11px]"></div>
+			</div>
+		</div>
+	)
+}
---
+++ web/scripts/index/Div_skeleton_portfolio.js
@@ -0,0 +1,17 @@
+function Div_skeleton_portfolio() {
+	return (
+		<div class="bg-gray-500 rounded-lg w-[144px] min-w-[144px] h-[160px]">
+
+			<div class="flex justify-between pb-[62px]">
+				<div class="bg-gray-400 rounded-full w-[32px] h-[32px] mt-[10px] ml-[10px] border-[2px] border-[#A4A4A4] border-solid"
+					 alt="profile picture"></div>
+			</div>
+		
+			<div class="ml-[10px] mb-[6px] mr-[10px] h-[14px] bg-gray-400 rounded-full"></div>
+			<div class="flex flex-row space-x-[6px] ml-[10px] mb-[6px]">
+				<button type="button" class="w-[20px] h-[20px] bg-gray-400 rounded-md"></button>
+				<button type="button" class="w-[36px] h-[20px] bg-gray-400 rounded-md"></button>
+			</div>
+		</div>
+	)
+}
