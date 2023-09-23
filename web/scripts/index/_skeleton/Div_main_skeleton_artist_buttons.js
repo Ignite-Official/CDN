ed5b1b7cba182b6410ec29c438a37d5146cf7d6f
Jaeseong Yoo <praster1@gmail.com>
2023-09-23 11:34:35 +0900

    지금, 새롭게 등장한 아티스트 - Skeleton - Buttons

---
+++ web/scripts/index/_skeleton/Div_main_skeleton_artist_buttons.js
@@ -0,0 +1,13 @@
+function Div_main_skeleton_artist_buttons() {
+	return (
+		<div class="flex flex-row justify-between items-center text-start w-full">
+			<div class="flex flex-row space-x-[8px] py-[24px]">
+				<button type="button" class="w-[48px] h-[25px] bg-gray-400 rounded-xl"></button>
+				<button type="button" class="w-[48px] h-[25px] bg-gray-400 rounded-xl"></button>
+				<button type="button" class="w-[48px] h-[25px] bg-gray-400 rounded-xl"></button>
+				<button type="button" class="w-[48px] h-[25px] bg-gray-400 rounded-xl"></button>
+				<button type="button" class="w-[48px] h-[25px] bg-gray-400 rounded-xl"></button>
+			</div>
+		</div>
+	)
+}
