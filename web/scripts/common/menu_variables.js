819233c5d1a7697f6e523557106af1506784c739
Jaeseong Yoo <praster1@gmail.com>
2023-10-01 11:20:28 +0900

    메뉴 스크립트

---
+++ web/scripts/common/menu_variables.js
@@ -0,0 +1,4 @@
+let toggle_menu = 0
+let toggle_alert = 0
+let class_alert_btn = "inline-flex justify-center items-center text-sm text-white rounded-lg w-[24px] h-[24px] cursor-pointer hover:bg-[#D9D9D9] hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200"
+let class_menu_btn = "inline-flex justify-center items-center text-sm text-white rounded-lg ml-[12px] w-[24px] h-[24px] cursor-pointer hover:bg-[#D9D9D9] hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200"
