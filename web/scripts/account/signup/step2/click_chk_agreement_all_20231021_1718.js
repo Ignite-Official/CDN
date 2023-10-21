67f0d9d6b1a51ea2cd0e3c97f21bf3cac8f15891
Jaeseong Yoo <praster1@gmail.com>
2023-10-21 17:21:53 +0900

    bug fix

---
+++ web/scripts/account/signup/step2/click_chk_agreement_all_20231021_1718.js
@@ -0,0 +1,14 @@
+function click_chk_agreement_all() {
+	if (toggle_chk_agreement) {
+		document.getElementById("chk_agreement").checked = document.getElementById("chk_agreement1").checked = document.getElementById("chk_agreement2").checked = false
+		toggle_chk_agreement = toggle_chk_agreement1 = toggle_chk_agreement2 = false
+
+		ReactDOM.render(<Div_btn_submit class_btn={class_btn_disabled} text={"다음"} function={null} />, document.getElementById("btn_submit"))
+
+	} else {
+		document.getElementById("chk_agreement").checked = document.getElementById("chk_agreement1").checked = document.getElementById("chk_agreement2").checked = true
+		toggle_chk_agreement = toggle_chk_agreement1 = toggle_chk_agreement2 = true
+
+		ReactDOM.render(<Div_btn_submit class_btn={class_btn_enabled} text={"다음"} function={() => click_step2_next()} />, document.getElementById("btn_submit"))
+	}
+}
