5936cf31a6ad460a6676eba7c97833b45d8a82ca
Jaeseong Yoo <praster1@gmail.com>
2023-11-02 03:23:15 +0900

    메뉴의 toggle을 true/false로 변경, 내 정보 추가

---
+++ web/scripts/common/menu/menu_variables_20231102_0321.js
@@ -0,0 +1,6 @@
+let toggle_hamburger = false
+let toggle_letter = false
+let toggle_alert = false
+let class_letter_btn = "inline-flex justify-center items-center text-sm text-white rounded-lg w-[24px] h-[24px] cursor-pointer hover:bg-[#D9D9D9] hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200"
+let class_alert_btn = "inline-flex justify-center items-center text-sm text-white rounded-lg w-[24px] h-[24px] cursor-pointer hover:bg-[#D9D9D9] hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200"
+let class_menu_btn = "inline-flex justify-center items-center text-sm text-white rounded-lg ml-[12px] w-[24px] h-[24px] cursor-pointer hover:bg-[#D9D9D9] hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-200"
