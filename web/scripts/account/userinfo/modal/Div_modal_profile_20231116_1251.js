816082f4d7b59322c86bdb94167dd78c242e2775
Jaeseong Yoo <praster1@gmail.com>
2023-11-16 13:10:14 +0900

    프로필 사진 모달, 커버 사진 모달에 함수 적용

---
+++ web/scripts/account/userinfo/modal/Div_modal_profile_20231116_1251.js
@@ -0,0 +1,29 @@
+function Div_modal_profile() {
+	return (
+		<div class={class_modal}>
+
+			<div class="relative bg-[#262626] rounded-lg shadow">
+				<Div_modal_title text={"프로필 변경"} />
+	
+				<div class="flex flex-col justify-center items-center w-full">
+					<div class={class_modal_btn_container}>
+						<button class={class_modal_btn_no} onClick={() => click_modal_profile_no()}>
+							기본 프로필 설정
+						</button>
+		
+						<button class={class_modal_btn_yes} onClick={() => click_modal_profile_yes()}>
+							새 이미지 업로드
+						</button>
+					</div>
+					
+					<div class={class_modal_div_close}>
+						<button class={class_modal_btn_close}
+								onClick={() => document.getElementById("btn_modal_profile_close").click()}>
+							닫기
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
