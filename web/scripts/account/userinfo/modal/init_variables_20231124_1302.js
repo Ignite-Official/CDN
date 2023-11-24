7a557aed29130ec40b76deeab50a9c968e04f69e
Jaeseong Yoo <praster1@gmail.com>
2023-11-24 13:02:37 +0900

    모달을 위한 초기 변수 추가

---
+++ web/scripts/account/userinfo/modal/init_variables_20231124_1302.js
@@ -0,0 +1,10 @@
+let class_modal = "relative w-[500px] min-w-[500px] max-w-[500px] min-h-1/3 max-h-1/3 p-[20px] space-y-[24px] sm:w-[360px] sm:min-w-[360px] sm:max-w-[360px] sm:p-[16px]"
+let class_modal_btn_container = "flex flex-row justyfy-center items-center w-full h-[100px] min-h-[150px] max-h-[150px] overflow-auto"
+let class_modal_btn_yes = "text-white bg-[#340BB8] font-medium rounded-lg text-sm text-center w-1/2 h-full hover:bg-[#26009F] hover:border hover:border-white"
+let class_modal_btn_no = "text-[#EDEDED] bg-transparent font-medium rounded-lg text-sm text-center w-1/2 h-full hover:border hover:border-white"
+let class_modal_div_close = "flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b w-full"
+let class_modal_btn_close = "text-white bg-[#340BB8] font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full hover:bg-[#26009F] focus:border focus:border-[#FFFFFF]"
+
+// 장르, 역할, 악기 선택 / 미선택 시 버튼
+let class_btn_act_notselected = "w-fit h-[40px] text-white font-[600] text-[14px] bg-transparent border border-2-[#525252] rounded-lg px-[16px] hover:bg-[#26009F]"
+let class_btn_act_selected = "w-fit h-[40px] text-white font-[600] text-[14px] bg-[#26009F] border border-2-[#525252] rounded-lg px-[16px] hover:bg-transparent"
