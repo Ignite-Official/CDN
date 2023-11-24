414f2f52f1bb8b6ecb5a5676d161ac3a901b6748
Jaeseong Yoo <praster1@gmail.com>
2023-11-24 11:46:10 +0900

    bug fix

---
+++ web/scripts/account/userinfo/modal/Div_modal_cover_20231124_1144.js
@@ -0,0 +1,31 @@
+function Div_modal_cover() {
+	return (
+		<div class={class_modal}>
+
+			<div class="relative bg-[#262626] rounded-lg shadow">
+				<Div_modal_title text={"커버 사진 변경"} />
+	
+				<div class="flex flex-col justify-center items-center w-full">
+					<div class={class_modal_btn_container}>
+						<button class={class_modal_btn_no} onClick={() => click_modal_cover_no()}>
+							기본 커버 이미지 설정
+						</button>
+		
+						<button class={class_modal_btn_yes} onClick={() => document.getElementById('id_file_input_cover').click()}>
+							새 이미지 업로드
+						</button>
+					</div>
+
+					<div class={class_modal_div_close}>
+						<button class={class_modal_btn_close}
+								onClick={() => document.getElementById("btn_modal_cover_close").click()}>
+							닫기
+						</button>
+					</div>
+				</div>
+			</div>
+			
+			<input type="file" name="id_file_input_cover" id="id_file_input_cover" class="hidden"  accept="image/*" onChange={() => check_file_upload_cover() }/>
+		</div>
+	)
+}
