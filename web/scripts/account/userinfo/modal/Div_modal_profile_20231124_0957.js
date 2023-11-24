82b6e590b69bc0be7542e4afa84bac885291cebf
Jaeseong Yoo <praster1@gmail.com>
2023-11-24 09:58:06 +0900

    함수 개수를 최소화

---
+++ web/scripts/account/userinfo/modal/Div_modal_profile_20231124_0957.js
@@ -0,0 +1,31 @@
+function Div_modal_profile() {
+	return (
+		<div class={class_modal}>
+
+			<div class="relative bg-[#262626] rounded-lg shadow">
+				<Div_modal_title text={"프로필 변경"} />
+	
+				<div class="flex flex-col justify-center items-center w-full">
+					<div class={class_modal_btn_container}>
+						<button class={class_modal_btn_no} onClick={() => click_modal_profile_no()}>
+							기본 프로필 설정
+						</button>
+		
+						<button class={class_modal_btn_yes} onClick={() => document.getElementById('id_file_input_profile').click()}>
+							새 이미지 업로드
+						</button>
+					</div>
+					
+					<div class={class_modal_div_close}>
+						<button class={class_modal_btn_close} id=""
+								onClick={() => document.getElementById("btn_modal_profile_close").click()}>
+							닫기
+						</button>
+					</div>
+				</div>
+			</div>
+
+			<input type="file" name="id_file_input_profile" id="id_file_input_profile" class="hidden"  accept="image/*" onChange={() => check_file_upload_profile() }/>
+		</div>
+	)
+}
