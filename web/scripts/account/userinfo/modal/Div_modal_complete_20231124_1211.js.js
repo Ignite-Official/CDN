d1c26e4c31b9b61dbedb903cb0e774110a556171
Jaeseong Yoo <praster1@gmail.com>
2023-11-24 12:12:10 +0900

    완료 버튼 클릭 시 이벤트

---
+++ web/scripts/account/userinfo/modal/Div_modal_complete_20231124_1211.js.js
@@ -0,0 +1,22 @@
+function Div_modal_complete() {
+	return (
+		<div class={class_modal}>
+
+			<div class="relative bg-[#262626] rounded-lg shadow">
+				<Div_modal_title text={"저장할까요?"} />
+	
+				<div class={class_modal_btn_container}>
+					<button class={class_modal_btn_yes}
+								 onClick={() => click_submit()}>
+						예
+					</button>
+	
+					<button class={class_modal_btn_no}
+							onClick={() => document.getElementById("btn_modal_complete_close").click()}>
+						아니오
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
