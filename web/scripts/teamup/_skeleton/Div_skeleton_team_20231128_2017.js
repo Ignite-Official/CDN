b7176655ebcf4bdc708aa4c49b8d9053a1dac4d2
Jaeseong Yoo <praster1@gmail.com>
2023-11-28 20:18:01 +0900

    bug fix

---
+++ web/scripts/teamup/_skeleton/Div_skeleton_team_20231128_2017.js
@@ -0,0 +1,30 @@
+function Div_skeleton_team(props) {
+	function Div_skeleton_card() {
+		return (
+			<div class="flex flex-row w-full justify-center items-center">
+				<div class="flex flex-col justify-start items-between bg-gray-500 rounded-lg animate-pulse
+							w-full max-w-[300px] h-[140px] min-h-[140px] max-h-[140px] mx-auto">
+				</div>
+			</div>
+		)
+	}
+
+	return (
+		<div class={props.class}>
+			<div id="div_team_counter">
+				<Div_skeleton_counter />
+			</div>
+		
+			<div id="div_team_filter" class="w-full">
+				<div class="flex flex-row space-x-[12px] justify-start items-center
+							md:grid md:grid-cols-2 md:space-x-[0px] md:justify-center md:gap-4">
+					<Div_skeleton_filter /><Div_skeleton_filter /><Div_skeleton_filter /><Div_skeleton_filter />
+				</div>
+			</div>
+		
+			<div class="grid grid-cols-4 w-full justify-center items-center gap-4 lg:grid-cols-2 md:grid-cols-1" id="div_team_sub">
+				<Div_skeleton_card /><Div_skeleton_card /><Div_skeleton_card /><Div_skeleton_card />
+			</div>
+		</div>
+	)
+}
