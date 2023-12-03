0eaf27855c531d020c9dddee6c2fa5cda5b5be9f
Jaeseong Yoo <praster1@gmail.com>
2023-12-03 09:17:58 +0900

    역할, 장르 표시 버튼

---
+++ web/scripts/common/components/Div_btn_act_22.js
@@ -0,0 +1,7 @@
+function Div_btn_act_22(props) {
+	return (
+		<p class="flex flex-row justify-center items-center bg-[#404040] text-white font-[400] text-[12px] w-fit h-[22px] px-[8px] rounded-md">
+			{props.name}
+		</p>
+	)
+}
