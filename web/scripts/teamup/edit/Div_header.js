1e877ae0f93ba97abea4ce44ccc752b45748b94d
Jaeseong Yoo <praster1@gmail.com>
2023-12-05 23:47:58 +0900

    팀 프로필 수정 헤더

---
+++ web/scripts/teamup/edit/Div_header.js
@@ -0,0 +1,9 @@
+function Div_header() {
+	return (
+		<div class="flex flex-col w-full justify-start items-center space-y-[4px]">
+			<span class="text-white font-[700] text-[24px] w-full text-start">
+				팀 프로필 수정
+			</span>
+		</div>
+	)
+}
