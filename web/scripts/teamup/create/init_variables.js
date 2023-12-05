e2f6583f49e45d7ed3d2cd8da76b0d17b9925cbd
Jaeseong Yoo <praster1@gmail.com>
2023-12-05 12:32:45 +0900

    팀 생성 - 완료 버튼 클릭 스크립트

---
+++ web/scripts/teamup/create/init_variables.js
@@ -0,0 +1,7 @@
+let data_genre = null
+let data_role = null
+let toggle_click_btn_submit = false
+
+// 장르, 역할, 악기 선택 / 미선택 시 버튼
+let class_btn_act_notselected = "w-fit h-[40px] text-white font-[600] text-[14px] bg-transparent border border-2-[#525252] rounded-lg px-[16px] hover:bg-[#26009F]"
+let class_btn_act_selected = "w-fit h-[40px] text-white font-[600] text-[14px] bg-[#26009F] border border-2-[#525252] rounded-lg px-[16px] hover:bg-transparent"
