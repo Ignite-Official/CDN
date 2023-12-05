588df4c319a7c810ed980888bdeba35007950ca5
Jaeseong Yoo <praster1@gmail.com>
2023-12-05 16:00:33 +0900

    팀 정보 가져오기

---
+++ web/scripts/teamup/team/get_teaminfo.js
@@ -0,0 +1,20 @@
+async function get_teaminfo() {
+	// 데이터 가져오기
+	const request_data = new FormData();
+	request_data.append('href', href);
+
+	teamdata = await fetch("/teamup/ajax_get_teaminfo/", {
+					 method: "post", 
+					 headers: { "X-CSRFToken": getCookie("csrftoken"), },
+					 body: request_data
+					 })
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	ReactDOM.render(<Div_header />, document.getElementById("div_header"))
+	ReactDOM.render(<Div_left />, document.getElementById("div_left"))
+	ReactDOM.render(<Div_right />, document.getElementById("div_right"))
+
+	get_member_list()   // 현재 멤버 목록
+	get_pending_list()  // 받은 요청 목록
+}
