e42c1103bb2a3a0458cb5a0bf04099184162c9f0
Jaeseong Yoo <praster1@gmail.com>
2023-12-05 20:17:53 +0900

    프로필 사진이 있을 경우, 프로필 사진으로 표시

---
+++ web/scripts/teamup/team/get_pending_list_20231205_2016.js
@@ -0,0 +1,37 @@
+async function get_pending_list() {
+	function Div_right_pending_list(props) {
+		const right_pending_List = Object.keys(props.data).map((btn_data) =>  
+			<Div_member_card account={props.data[btn_data].account}
+							 nickname={props.data[btn_data].nickname}
+							 url_image_profile={props.data[btn_data].url_image_profile}
+							 gender={props.data[btn_data].gender}
+							 total_cnt={props.data[btn_data].total_cnt}
+							 sum_respect={props.data[btn_data].sum_respect}
+							 txt_intro={props.data[btn_data].txt_intro}
+							 member_type={props.data[btn_data].member_type}
+							 type={"pending"}
+							/>
+		)
+
+		return (
+			<div class="grid grid-cols-3 justify-center items-center w-full gap-[24px] md:grid-cols-2 sm:md:grid-cols-1">
+				{right_pending_List}
+			</div>
+		)
+	}
+
+	// 데이터 가져오기
+	const request_data = new FormData();
+	request_data.append('href', href);
+
+	let teamdata_pending_list = await fetch("/teamup/ajax_get_team_pending_list/", {
+								  method: "post", 
+								  headers: { "X-CSRFToken": getCookie("csrftoken"), },
+								  body: request_data
+								  })
+								  .then(res=> { return res.json(); })
+								  .then(res=> { return res; });
+	teamdata['pending_list'] = teamdata_pending_list['pending_list']
+
+	ReactDOM.render(<Div_right_pending_list data={teamdata['pending_list']} />, document.getElementById("div_right_pending_list"))
+}
