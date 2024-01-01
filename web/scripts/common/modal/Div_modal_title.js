807558cecee4bcee550bb8e18a092496b80a33bb
Jaeseong Yoo <praster1@gmail.com>
2024-01-01 17:57:46 +0900

    common modal scripts

---
+++ web/scripts/common/modal/Div_modal_title.js
@@ -0,0 +1,9 @@
+function Div_modal_title(props) {
+	return (
+		<div class="flex items-center justify-center w-full h-[50px] px-[20px] py-[8px] border-b rounded-t">
+			<span class="text-[#D4D4D4] font-[600] text-[16px] w-full">
+				{props.text}
+			</span>
+		</div>   
+	)
+}
---
+++ web/scripts/common/modal/init_variables.js
@@ -0,0 +1,6 @@
+let class_modal = "relative w-[500px] min-w-[500px] max-w-[500px] min-h-1/3 max-h-1/3 p-[20px] space-y-[24px] sm:w-[360px] sm:min-w-[360px] sm:max-w-[360px] sm:p-[16px]"
+let class_modal_btn_container = "flex flex-row justyfy-center items-center w-full h-[100px] min-h-[150px] max-h-[150px] overflow-auto"
+let class_modal_btn_yes = "text-white bg-[#340BB8] font-medium rounded-lg text-sm text-center w-1/2 h-full hover:bg-[#26009F] hover:border hover:border-white"
+let class_modal_btn_no = "text-[#EDEDED] bg-transparent font-medium rounded-lg text-sm text-center w-1/2 h-full hover:border hover:border-white"
+let class_modal_div_close = "flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b w-full"
+let class_modal_btn_close = "text-white bg-[#340BB8] font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full hover:bg-[#26009F] focus:border focus:border-[#FFFFFF]"
