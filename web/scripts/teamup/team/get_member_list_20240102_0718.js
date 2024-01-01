d620e99f4447334e32aeaf6212334dd2c3971bf7
Jaeseong Yoo <praster1@gmail.com>
2024-01-02 07:19:21 +0900

    bug fix

---
+++ web/scripts/teamup/team/get_member_list_20240102_0718.js
@@ -0,0 +1,50 @@
+async function get_member_list() {
+  function Div_right_member_list(props) {
+		const right_member_List = Object.keys(props.data).map((btn_data) =>  
+			   <Div_member_card account={props.data[btn_data].account}
+									nickname={props.data[btn_data].nickname}
+									url_image_profile={props.data[btn_data].url_image_profile}
+									gender={props.data[btn_data].gender}
+									total_cnt={props.data[btn_data].total_cnt}
+									sum_respect={props.data[btn_data].sum_respect}
+									txt_intro={props.data[btn_data].txt_intro}
+									member_type={props.data[btn_data].member_type}
+									type={"member"} />
+		)
+
+		return (
+			   <div class="grid grid-cols-3 justify-center items-start w-full gap-[24px] md:grid-cols-2 sm:md:grid-cols-1">
+					 {right_member_List}
+			   </div>
+		)
+  }
+
+  function Div_right_member_empty() {
+		return (
+			   <p class="flex flex-row justify-center items-center w-full text-[#A3A3A3] font-[600] text-[16px]">
+					 가입한 멤버가 없습니다.
+			   </p>
+		)
+  }
+
+  // 데이터 가져오기
+  const request_data = new FormData();
+  request_data.append('href', href);
+
+  let teamdata_member_list = await fetch("/teamup/ajax_get_team_member_list/", {
+													method: "post", 
+													headers: { "X-CSRFToken": getCookie("csrftoken"), },
+													body: request_data
+													})
+													.then(res=> { return res.json(); })
+													.then(res=> { return res; });
+  teamdata['member_list'] = teamdata_member_list['member_list']
+
+  document.getElementById("tab_right_member_list").innerHTML = "현재 멤버(" + Object.keys(teamdata['member_list']).length + ")"
+  if (Object.keys(teamdata['member_list']).length > 0) {
+		ReactDOM.render(<Div_right_member_list data={teamdata['member_list']} />, document.getElementById("div_right_member_list"))
+  } else {
+		ReactDOM.render(<Div_right_member_empty />, document.getElementById("div_right_member_list"))
+  }
+  
+}
---
+++ web/scripts/teamup/team/get_sending_list_20240102_0718.js
@@ -0,0 +1,49 @@
+async function get_sending_list() {
+  function Div_right_sending_list(props) {
+	   const right_sending_List = Object.keys(props.data).map((btn_data) =>  
+			<Div_member_card account={props.data[btn_data].account}
+								nickname={props.data[btn_data].nickname}
+								url_image_profile={props.data[btn_data].url_image_profile}
+								gender={props.data[btn_data].gender}
+								total_cnt={props.data[btn_data].total_cnt}
+								sum_respect={props.data[btn_data].sum_respect}
+								txt_intro={props.data[btn_data].txt_intro}
+								member_type={props.data[btn_data].member_type}
+								type={"sending"} />
+	   )
+
+	   return (
+			<div class="grid grid-cols-3 justify-center items-center w-full gap-[24px] md:grid-cols-2 sm:md:grid-cols-1">
+				 {right_sending_List}
+			</div>
+	   )
+  }
+
+  function Div_right_sending_empty() {
+	   return (
+			<p class="flex flex-row justify-center items-center w-full text-[#A3A3A3] font-[600] text-[16px]">
+				 보낸 요청이 없습니다.
+			</p>
+	   )
+  }
+
+  // 데이터 가져오기
+  const request_data = new FormData();
+  request_data.append('href', href);
+
+  let teamdata_sending_list = await fetch("/teamup/ajax_get_team_sending_list/", {
+											   method: "post", 
+											   headers: { "X-CSRFToken": getCookie("csrftoken"), },
+											   body: request_data
+											   })
+											   .then(res=> { return res.json(); })
+											   .then(res=> { return res; });
+  teamdata['sending_list'] = teamdata_sending_list['sending_list']
+
+  document.getElementById("tab_right_sending_list").innerHTML = "보낸 요청(" + Object.keys(teamdata['sending_list']).length + ")"
+  if (Object.keys(teamdata['sending_list']).length > 0) {
+	   ReactDOM.render(<Div_right_sending_list data={teamdata['sending_list']} />, document.getElementById("div_right_sending_list"))
+  } else {
+	   ReactDOM.render(<Div_right_sending_empty />, document.getElementById("div_right_sending_list"))
+  }
+}
