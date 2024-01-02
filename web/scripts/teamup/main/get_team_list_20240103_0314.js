0885b538acbc1d496be5d1427e915932980d3297
Jaeseong Yoo <praster1@gmail.com>
2024-01-03 03:15:23 +0900

    카드 form 변경에 따른 추가

---
+++ web/scripts/teamup/main/get_team_list_20240103_0314.js
@@ -0,0 +1,40 @@
+async function get_team_list() {
+	function Div_team_sub(props) {
+		const card_List = Object.keys(props.data).map((btn_data) =>  
+			<Div_team_card name={props.data[btn_data].name}
+						   url_team={props.data[btn_data].url_team}
+						   due_date_remain={props.data[btn_data].due_date_remain}
+						   role={props.data[btn_data].role}
+						   total_cnt={props.data[btn_data].total_cnt}
+						   role_name={props.data[btn_data].role_name}
+						   nickname_creator={props.data[btn_data].nickname_creator}
+						   url_image_profile_creator={props.data[btn_data].url_image_profile_creator}
+						   teamup_available={props.data[btn_data].teamup_available} />
+
+		)
+
+		return (
+			<div class="grid grid-cols-4 w-full justify-center items-center gap-4 lg:grid-cols-2 md:grid-cols-1">
+				{card_List}
+			</div>
+		)
+	}
+
+	const data = await fetch("/teamup/ajax_get_team_list/", {
+								method: "post", 
+								headers: {"X-CSRFToken": getCookie("csrftoken"),}
+							})
+					.then(res=> { return res.json(); })
+					.then(res=> { return res; });
+
+	if (gv_username == "") {
+		ReactDOM.render(<Div_counter title={"팀"}
+									 counter={Object.keys(data).length.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + "건"} />, document.getElementById("div_team_counter"))
+	} else {
+		ReactDOM.render(<Div_counter title={"팀"}
+									 counter={Object.keys(data).length.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",") + "건"} 
+									 button={"true"} />, document.getElementById("div_team_counter"))
+	}
+
+	ReactDOM.render(<Div_team_sub data={data} />, document.getElementById("div_team_sub"))
+}
