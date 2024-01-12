3d780b6f8a5841b02f321528af5ba18d6473e635
Jaeseong Yoo <praster1@gmail.com>
2024-01-12 09:11:11 +0900

    필터 division의 hidden 부분 제거

---
+++ web/scripts/teamup/main/set_main_20240112_0910.js
@@ -0,0 +1,41 @@
+function set_main(url) {
+	function Div_main() {
+		return (
+			<div class="w-full">
+				<div id="div_joined" class="w-full"></div>
+				<div id="div_selection" class="w-full"></div>
+				<div id="div_artist" class="w-full"></div>
+				<div id="div_team" class="w-full"></div>
+			</div>
+		)
+	}
+
+	ReactDOM.render(<Div_main />, document.getElementById("div_main"))
+	ReactDOM.render(<Div_skeleton_joined />, document.getElementById("div_joined"))
+	ReactDOM.render(<Div_selection />, document.getElementById("div_selection"))
+	ReactDOM.render(<Div_skeleton_artist />, document.getElementById("div_artist"))
+	ReactDOM.render(<Div_skeleton_team />, document.getElementById("div_team"))
+	
+	// 내가 가입한 팀 목록이, 로그인 여부에 따라 달리 보이게 됨
+	if (gv_username == "") {
+		document.getElementById("div_joined_counter").className = "hidden"
+		document.getElementById("div_joined_content").className = "hidden"
+
+	} else {
+		document.getElementById("div_joined_not_login").className = "hidden"
+	}
+	
+	if (url == "team") {
+		document.getElementById("div_artist").className = "hidden"
+		document.getElementById("tab_artist").className = class_tab_off
+		document.getElementById("tab_team").className = class_tab_on
+	} else {
+		document.getElementById("div_team").className = "hidden"
+		document.getElementById("tab_artist").className = class_tab_on
+		document.getElementById("tab_team").className = class_tab_off
+	}
+
+	get_my_team_list()
+	get_artist_list()
+	get_team_list()
+}
