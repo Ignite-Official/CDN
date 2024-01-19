31c2076039cef1594078446fc69f748795a61ea0
Jaeseong Yoo <praster1@gmail.com>
2024-01-19 14:14:05 +0900

    click_tab 시 토글 변수의 값 변경

---
+++ web/scripts/teamup/main/click_tab_20240119_1413.js
@@ -0,0 +1,19 @@
+function click_tab(tab) {
+	toggle_tab = tab
+
+	if (tab == "artist") {
+		document.getElementById("div_artist").className = "w-full"
+		document.getElementById("tab_artist").className = class_tab_on
+
+		document.getElementById("div_team").className = "hidden"
+		document.getElementById("tab_team").className = class_tab_off
+
+	
+	} else {
+		document.getElementById("div_artist").className = "hidden"
+		document.getElementById("tab_artist").className = class_tab_off
+
+		document.getElementById("div_team").className = "w-full"
+		document.getElementById("tab_team").className = class_tab_on
+	}
+}
